f98de2674acad48ed19cd3f41a7602e81dc34b7e
Felipe Prodóssimo <73621753+FelipeProdossimo@users.noreply.github.com>
2021-06-21 22:17:03 -0300

    3 Projetos Add, IMC, Estacionamento e Matricula!

---
+++ EstacionamentoIbira/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace EstacionamentoIbira
+{
+    partial class frmIbira
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtHoraEntrada = new System.Windows.Forms.TextBox();
+            this.txtHoraSaida = new System.Windows.Forms.TextBox();
+            this.txtTotalPagar = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(208, 78);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(100, 23);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "CALCULAR";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "HORA DE ENTRADA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "HORA DE SAÍDA";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(205, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "TOTAL A PAGAR";
+            // 
+            // txtHoraEntrada
+            // 
+            this.txtHoraEntrada.BackColor = System.Drawing.Color.Green;
+            this.txtHoraEntrada.Location = new System.Drawing.Point(31, 23);
+            this.txtHoraEntrada.Name = "txtHoraEntrada";
+            this.txtHoraEntrada.Size = new System.Drawing.Size(100, 20);
+            this.txtHoraEntrada.TabIndex = 4;
+            // 
+            // txtHoraSaida
+            // 
+            this.txtHoraSaida.BackColor = System.Drawing.Color.Red;
+            this.txtHoraSaida.Location = new System.Drawing.Point(31, 81);
+            this.txtHoraSaida.Name = "txtHoraSaida";
+            this.txtHoraSaida.Size = new System.Drawing.Size(100, 20);
+            this.txtHoraSaida.TabIndex = 5;
+            // 
+            // txtTotalPagar
+            // 
+            this.txtTotalPagar.BackColor = System.Drawing.Color.Yellow;
+            this.txtTotalPagar.Location = new System.Drawing.Point(208, 23);
+            this.txtTotalPagar.Name = "txtTotalPagar";
+            this.txtTotalPagar.Size = new System.Drawing.Size(100, 20);
+            this.txtTotalPagar.TabIndex = 6;
+            // 
+            // frmIbira
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtTotalPagar);
+            this.Controls.Add(this.txtHoraSaida);
+            this.Controls.Add(this.txtHoraEntrada);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCalcular);
+            this.Name = "frmIbira";
+            this.Text = "ESTACIONAMENTO IBIRA";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtHoraEntrada;
+        private System.Windows.Forms.TextBox txtHoraSaida;
+        private System.Windows.Forms.TextBox txtTotalPagar;
+    }
+}
+
---
+++ EstacionamentoIbira/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EstacionamentoIbira
+{
+    public partial class frmIbira : Form
+    {
+        public frmIbira()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double hInicio = Convert.ToDouble(txtHoraEntrada.Text);
+            double hFim = Convert.ToDouble(txtHoraSaida.Text);
+            double totalHora = (hFim - hInicio) * 5;
+
+            txtTotalPagar.Text = totalHora.ToString("N");
+
+        }
+    }
+}
